d9df4d4f56898e130730d1736d586400069cd714
sknibbsascendfs <51423408+sknibbsascendfs@users.noreply.github.com>
2020-03-14 13:47:21 -0400

    WP2Static CREATE doc/wp-content/themes/ascendfs-5050/assets/js/swiper-call.js

---
+++ doc/wp-content/themes/ascendfs-5050/assets/js/swiper-call.js
@@ -0,0 +1,23 @@
+var swiper = new Swiper('.swiper-container.mainslider', {
+	initialSlide:0,
+    effect: 'fade',
+    preloadImages: false,
+    // Enable lazy loading
+    lazy: true,
+    autoplay: {
+        delay: 5000,
+    }
+    
+    
+
+});
+ var swiperV = new Swiper('.swiper-container.swiper-slide', {
+       
+        height:'700px',
+                
+    });
+
+$(document).ready(function() {
+    var height = $(window).height();
+    $(".slider .swiper-slide").css("height", height-50);
+});
