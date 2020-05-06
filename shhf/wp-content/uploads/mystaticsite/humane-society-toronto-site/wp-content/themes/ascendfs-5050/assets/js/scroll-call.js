1bd4db1586a5b6e5633af8156fee1e56aeea10df
sknibbsascendfs <51423408+sknibbsascendfs@users.noreply.github.com>
2020-05-06 14:17:16 -0400

    South Huron Hospital Foundation

---
+++ shhf/wp-content/uploads/mystaticsite/humane-society-toronto-site/wp-content/themes/ascendfs-5050/assets/js/scroll-call.js
@@ -0,0 +1,13 @@
+$(window).scroll(function() {
+
+    var scroll = $(window).scrollTop();
+      
+    /*if (scroll > 1080) {
+        $('.slider-navigation-link').addClass("hidden");
+        $('.slider-navigation-link').removeClass("visible");
+    } else {
+        $('.slider-navigation-link').addClass("visible");
+        $('.slider-navigation-link').removeClass("hidden");
+    }*/
+ 
+});
